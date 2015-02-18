4183c8773502c43ffc52cd880c0e624db9b5374b
Tom Janssens <tom@corebvba.be>
2015-02-18 11:31:29 +0100

    First commit

---
+++ HaskellTypeClasses.Specs/maybe.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace HaskellTypeClasses.Specs
+{
+    [TestClass]
+    public class MaybeSpecs
+    {
+
+        [TestMethod]
+        public void Nothing_isJust_should_be_false()
+        {
+            Maybe.Nothing<int>().isJust.ShouldBe(false);
+        }
+
+        [TestMethod]
+        public void A_Just_isJust_should_be_true()
+        {
+            Maybe.Just(1).isJust.ShouldBe(true);
+        }
+
+        [TestMethod]
+        public void Nothing_isNothing_should_be_true()
+        {
+            Maybe.Nothing<int>().isNothing.ShouldBe(true);
+        }
+
+        [TestMethod]
+        public void A_Just_isNothing_should_be_false()
+        {
+            Maybe.Just(1).isNothing.ShouldBe(false);
+        }
+
+        [TestMethod]
+        public void This_is_the_regular_way_you_would_use_a_maybe()
+        {
+            var result = Maybe.Just("25")
+                .Bind(
+                    s =>
+                    {
+                        // return int if parsable
+                        int i = 0;
+                        return int.TryParse(s, out i) ? Maybe.Just(i) : Maybe.Nothing<int>();
+                    },
+                    i =>
+                        // return sqrt if > 0
+                        i < 0 ? Maybe.Nothing<double>() : Maybe.Just(Math.Sqrt((double)i))
+                ).Do(
+                    d =>
+                        // print
+                        Console.WriteLine(d)
+                ).Bind(
+                    d =>
+                        // subtract 2
+                        Maybe.Just(d - 2.0)
+                );
+
+            // one should never actually use this, but use do & bind IRL
+            result.isJust.ShouldBe(true);
+            result.fromJust.ShouldBe(3.0);
+        }
+
+
+        [TestMethod]
+        public void A_nothing_should_not_invoke_any_functions()
+        {
+            var methods = new Methods();
+
+            var result = Maybe.Nothing<string>()
+                .Bind(
+                    methods.Parse_string_as_int,
+                    methods.Squareroot_int
+                ).Do(
+                    methods.Print_double
+                ).Bind(
+                    methods.Subtract_2
+                );
+
+            result.ShouldBe(Maybe.Nothing<double>());
+
+            methods.Parse_string_as_int_was_called.ShouldBe(false);
+            methods.Squareroot_int_was_called.ShouldBe(false);
+            methods.Print_double_was_called.ShouldBe(false);
+            methods.Subtract_2_was_called.ShouldBe(false);
+        }
+
+        [TestMethod]
+        public void A_non_parsable_string_should_return_a_nothing()
+        {
+            var methods = new Methods();
+
+            var result = Maybe.Just("@ToJans")
+                .Bind(
+                    methods.Parse_string_as_int,
+                    methods.Squareroot_int
+                ).Do(
+                    methods.Print_double
+                ).Bind(
+                    methods.Subtract_2
+                );
+
+            result.ShouldBe(Maybe.Nothing<double>());
+
+            methods.Parse_string_as_int_was_called.ShouldBe(true);
+            methods.Squareroot_int_was_called.ShouldBe(false);
+            methods.Print_double_was_called.ShouldBe(false);
+            methods.Subtract_2_was_called.ShouldBe(false);
+        }
+
+        [TestMethod]
+        public void A_parsable_negative_int_should_return_a_nothing()
+        {
+            var methods = new Methods();
+
+            var result = Maybe.Just("-5")
+                .Bind(
+                    methods.Parse_string_as_int,
+                    methods.Squareroot_int
+                ).Do(
+                    methods.Print_double
+                ).Bind(
+                    methods.Subtract_2
+                );
+
+            result.ShouldBe(Maybe.Nothing<double>());
+
+            methods.Parse_string_as_int_was_called.ShouldBe(true);
+            methods.Squareroot_int_was_called.ShouldBe(true);
+            methods.Print_double_was_called.ShouldBe(false);
+            methods.Subtract_2_was_called.ShouldBe(false);
+        }
+
+
+        [TestMethod]
+        public void A_parsable_positive_int_should_return_it_s_squareroot()
+        {
+            var methods = new Methods();
+
+            var result = Maybe.Just("25")
+                .Bind(
+                    methods.Parse_string_as_int,
+                    methods.Squareroot_int
+                ).Do(
+                    methods.Print_double
+                ).Bind(
+                    methods.Subtract_2
+                );
+
+
+            result.ShouldBe(Maybe.Just(3.0));
+
+            methods.Parse_string_as_int_was_called.ShouldBe(true);
+            methods.Squareroot_int_was_called.ShouldBe(true);
+            methods.Print_double_was_called.ShouldBe(true);
+            methods.Subtract_2_was_called.ShouldBe(true);
+        }
+
+        class Methods
+        {
+            public Methods() { }
+
+            public bool Parse_string_as_int_was_called = false;
+            public bool Squareroot_int_was_called = false;
+            public bool Print_double_was_called = false;
+            public bool Subtract_2_was_called = false;
+
+            public Maybe.Monad<int> Parse_string_as_int(string s)
+            {
+                Parse_string_as_int_was_called = true;
+                var i = 0;
+                return int.TryParse(s, out i) ? Maybe.Just(i) : Maybe.Nothing<int>();
+            }
+
+            public Maybe.Monad<double> Squareroot_int(int i)
+            {
+                Squareroot_int_was_called = true;
+                return i < 0 ? Maybe.Nothing<double>() : Maybe.Just(Math.Sqrt((double)i));
+            }
+
+            public void Print_double(double d)
+            {
+                Print_double_was_called = true;
+                Console.WriteLine("output: %d", d);
+            }
+
+            public Maybe.Monad<double> Subtract_2(double d)
+            {
+                Subtract_2_was_called = true;
+                return Maybe.Just(d - 2.0);
+            }
+
+
+        }
+
+
+
+
+    }
+}
---
+++ HaskellTypeClasses/Maybe.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace HaskellTypeClasses
+{
+    public class Maybe
+    {
+        public static Monad<A> Nothing<A>() { return Monad<A>.Nothing; }
+        public static Monad<A> Just<A>(A a) { return Monad<A>.Just(a); }
+
+        public class Monad<A>
+        {
+            public static readonly Monad<A> Nothing = new Monad<A>(default(A));
+
+            private A a;
+
+            private Monad(A a)
+            {
+                this.a = a;
+            }
+
+            public static Monad<A> Just(A a)
+            {
+                return new Monad<A>(a);
+            }
+
+            public bool isNothing { get { return this == Nothing; } }
+
+            public bool isJust { get { return !isNothing; } }
+
+            public A fromJust { get { if (isJust) return a; else throw new InvalidOperationException(); } }
+
+            public A fromMaybe(A @default)
+            {
+                return isJust ? fromJust : @default;
+            }
+
+            public override int GetHashCode()
+            {
+                return (a==null?98654132:a.GetHashCode()) ^ isJust.GetHashCode();
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (this == obj) return true;
+                if (obj == Monad<A>.Nothing && this != Monad<A>.Nothing) return false;
+                var c = obj as Monad<A>;
+                if (c == null) return false;
+                if (c.a == null && this.a != null) return false;
+                return c.a.Equals(this.a);
+            }
+
+            public Monad<B> Bind<B>(
+                Func<A, Monad<B>> ab
+                )
+            {
+                return this.isNothing ? Monad<B>.Nothing : ab(this.a);
+            }
+
+            public Monad<C> Bind<B, C>(
+                Func<A, Monad<B>> ab,
+                Func<B, Monad<C>> bc)
+            {
+                return this.Bind(ab).Bind(bc);
+            }
+
+            public Monad<D> Bind<B, C, D>(
+                Func<A, Monad<B>> ab,
+                Func<B, Monad<C>> bc,
+                Func<C, Monad<D>> cd)
+            {
+                return this.Bind(ab, bc).Bind(cd);
+            }
+
+            public Monad<E> Bind<B, C, D, E>(
+                Func<A, Monad<B>> ab,
+                Func<B, Monad<C>> bc,
+                Func<C, Monad<D>> cd,
+                Func<D, Monad<E>> de)
+            {
+                return this.Bind(ab, bc, cd).Bind(de);
+            }
+
+            public Monad<A> Do(Action<A> action)
+            {
+                if (this.isJust)
+                {
+                    action(this.a);
+                }
+                return this;
+            }
+
+        }
+    }
+}
